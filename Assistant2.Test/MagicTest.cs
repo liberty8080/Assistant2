c4353cb7bdbfc77205ff8cbfca58de761793b82e
liberty8080 <zhaojiawei233@gmail.com>
2022-06-28 13:35:46 +0800

    rocket regex

--- Assistant2.Test/MagicTest.cs
+++ Assistant2.Test/MagicTest.cs
@@ -20,4 +20,17 @@ public class MagicTest
         songuo.Update(ref sub);
         Console.WriteLine(sub);
     }
+
+    [Test]
+    public void ParseTest()
+    {
+        const string status = "剩余流量：9.72TB.♥.2022-07-05";
+        var s = new MagicSubscribe
+        {
+            RocketRegex = @"剩余流量：(?<bandwidth>\d\.\d{2}(GB|TB|MB)).♥.(?<expire>\d{4}-\d{2}-\d{2})"
+        };
+        RocketUpdater.ParseInfo(ref s,status);
+        Console.WriteLine(s.BandwidthLeft);
+        Console.WriteLine(s.ExpirationTime);
+    }
 }
--- Assistant2.server/Services/Magic/RocketUpdater.cs
+++ Assistant2.server/Services/Magic/RocketUpdater.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using Assistant2.Exceptions;
 using Assistant2.Models;
 using Assistant2.Util;
 
@@ -6,14 +7,14 @@ namespace Assistant2.Services.Magic;
 
 public class RocketUpdater : BaseUpdater
 {
-    private readonly string _reg = @"^STATUS=(.*)\nREMARKS=.*\n";
-    
+    private const string Reg = @"^STATUS=(.*)\nREMARKS=.*\n";
+
     public override void Update(ref MagicSubscribe subscribe)
     {
         if (subscribe.Data != string.Empty)
         {
             var rowData = MagicUtil.DecodeBase64(subscribe.Data);
-            var matches = Regex.Match(rowData, _reg);
+            var matches = Regex.Match(rowData, Reg);
             var status = matches.Groups[1].Value;
             // var remarks = matches.Groups[2].Value;
         }
@@ -23,13 +24,15 @@ public class RocketUpdater : BaseUpdater
     }
 
     // parse expire time and flow 
-    private static void ParseInfo(ref MagicSubscribe subscribe,string target)
+    public static void ParseInfo(ref MagicSubscribe subscribe, string target)
     {
-        var m =Regex.Match(target, subscribe.RocketRegex);
-        if (!m.Groups[1].Success||!m.Groups[2].Success)
+        var m = Regex.Match(target, subscribe.RocketRegex);
+        if (!m.Groups[1].Success || !m.Groups[2].Success)
         {
-            
+            throw new MagicException($"sub info parse failed! original string is:{target}" +
+                                     "use named regex: [bandwidth,expire]");
         }
-
+        subscribe.BandwidthLeft = m.Groups["bandwidth"].Value;
+        subscribe.ExpirationTime = m.Groups["expire"].Value;
     }
 }
